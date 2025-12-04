575a7860d88b05b2db6b1f15e0002c5ca02ad445
minalll12 <minal007.biju@gmail.com>
2025-12-04 15:42:23 +0800

    editt

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class chatRequest(BaseModel):
+    prompt: str
+
+class chatResponse(BaseModel):
+    response: str
 
 app = FastAPI()
 
 @app.get("/")
 def health():
     return {"status": "Ok! This is working woohoo!"}
+
+@app.post("/chat", response_model=chatResponse)
+def chat(request: chatRequest):
+     return chatResponse(response=f"I will get smarter later!")  
